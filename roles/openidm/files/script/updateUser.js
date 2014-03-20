67f816d160a959aaa3c7cb375e1d1c2a1435472b
Warren Strange <warren.strange@gmail.com>
2014-03-20 17:04:16 -0600

    added sample openidm files

--- roles/openidm/files/script/updateUser.js
+++ roles/openidm/files/script/updateUser.js
@@ -2,52 +2,56 @@
 response = {};
 
 //read in parameters
-id = request.params['id'];
-name =  request.params['name'];
+id = request.value['id'];
+patch = request.value['patch'];
 
 path = "managed/user/" + id;
 
 //find user using get-by-field-value using email address
 user = openidm.read(path);
 
-logger.error("Read user {}  request value {} name {}", path, request.value, name);
+logger.info("Read user {}  request value {} patch {}", path, request.value,
+		patch);
 
-// define functions we need 
+// define functions we need
 function sendEmail(email, message) {
-	var params =  new Object();
+	var params = new Object();
 	params._from = "openidm@example.com";
-	params._to = email;	
+	params._to = email;
 	params._subject = "Account updated";
-	params._body = "Your account details have been updated " + message;	 
+	params._body = message;
 	openidm.action("external/email", params);
+}
+
+// Return true if we should send out a change notification to the user
+function shouldSendNotification(user, patchList) {
+	for (var i = 0; i < patchList.length; ++i) {
+		p = patchList[i];
+		if (p["replace"] == "/password") {
+			logger.info("Password attribute updated - will send email");
+			return true;
+		}
+	}
+	return false;
 }
 
 if (user) {
 	// patch list
-	r = [{ "replace": "/givenName", "value": name}];
-	revision = (parseInt(user._rev) +1).toString();
+	revision = (parseInt(user._rev) + 1).toString();
 	try {
-		response.result = openidm.patch(path,null,r);
-		sendEmail(user.email,"Name updated to " + name);
-	}
-	catch(err) {
-		logger.error("Error on update {}. Will rollback change to {}", err,path);		
+		response.result = openidm.patch(path, null, patch);
+		if (shouldSendNotification(user, patch))
+			sendEmail(
+					user.email,
+					"Your account has been updated. If you did not make these changes please call TELUS ");
+	} catch (err) {
+		logger.error("Error on update {}. Rollback change to {}", err, path);
 		response.result = "error on update. Error =" + err;
-         try {
-             // this will also fail if a target is down, but it will
-             // put the managed user back to the way it was
-             res2 = openidm.update(path,revision,user);
-             logger.error("Result of rolback {}", res2);
-         }
-         catch(err2) {
-        	 // we ignore this error - because we expect the update to fail
-        	 logger.error("expected error {}", err);
-         }
-	}	
+	}
 }
 //if user doesn't exist
 else {
-	response.comment = "Invalid id";	
+	response.comment = "Invalid id";
 }
 
 //send result back
