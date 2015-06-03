4e512d352887effe0f3f0f23d63b77c158c91a14
Warren Strange <warren.strange@gmail.com>
2015-06-03 12:33:46 -0600

    ssoconfig

--- docker/k8/gce/bounce.sh
+++ docker/k8/gce/bounce.sh
@@ -2,14 +2,14 @@
 
 # Delete all - but dont stop the cluster
 
-
-
+echo Deleting containers
+kubectl delete -f ssoconfig-controller.yaml
 kubectl delete -f opendj-controller.yaml
 kubectl delete -f openam-controller.yaml
 kubectl delete -f openam-controllerb.yaml
 
 
-
+echo Re-creating containers
 kubectl create -f opendj-controller.yaml
 kubectl create -f openam-controller.yaml
 kubectl create -f openam-controllerb.yaml
